9889c87265dc0319c1754ddc322405818de19d0f
Wisdom-oyemi <oyemiwisdom@gmail.com>
2026-02-09 20:09:53 -0500

    Added file categories and pre-code imports

---
+++ organizer.py
@@ -0,0 +1,10 @@
+FILE_CATEGORIES = {
+    "PDFs": [".pdf"],
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Archives": [".zip", ".7z", ".rar", ".tar", ".gz"],
+    "Code": [".py", ".java", ".cpp", ".c", ".js", ".ts", ".html", ".css"],
+}
+
+import os
+from pathlib import Path
+import shutil
