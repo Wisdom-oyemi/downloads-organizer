c41bec813e632a1ab3637a16199fed123439971a
Wisdom-oyemi <oyemiwisdom@gmail.com>
2026-02-15 14:17:51 -0500

    FINALLY implemented a feature - read path input and creates folder

--- organizer.py
+++ organizer.py
@@ -6,5 +6,30 @@ FILE_CATEGORIES = {
 }
 
 import os
-from pathlib import Path
 import shutil
+import sys
+
+folder_path = input("Enter file path: ").strip('/"')
+decoded_folder_path = os.fsdecode(folder_path)
+os.chdir(f'{decoded_folder_path}')
+os.makedirs('Organizer-Folder/SubFolder-1')
+
+
+'''
+def create_folders(dir_path, FILE_CATEGORIES):
+    for key in FILE_CATEGORIES:
+        if key not in 
+    pass
+
+
+def get_files(filepath):
+    for file in os.listdir(filepath):
+        filename = os.fsdecode(file)
+        print(filename)
+
+
+if __name__ == "__main__":
+    filepath = sys.argv[0]
+    get_files(filepath)
+
+'''
