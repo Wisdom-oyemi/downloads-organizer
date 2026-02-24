8cccbf8a6190fd4e8b767d90adab41b71921e1b8
Wisdom-oyemi <oyemiwisdom@gmail.com>
2026-02-23 23:00:51 -0500

    ported feature + rearranged the code

--- organizer.py
+++ organizer.py
@@ -1,35 +1,23 @@
-FILE_CATEGORIES = {
-    "PDFs": [".pdf"],
-    "Images": [".jpg", ".jpeg", ".png", ".gif"],
-    "Archives": [".zip", ".7z", ".rar", ".tar", ".gz"],
-    "Code": [".py", ".java", ".cpp", ".c", ".js", ".ts", ".html", ".css"],
-}
+
 
 import os
 import shutil
 import sys
 
-folder_path = input("Enter file path: ").strip('/"')
-decoded_folder_path = os.fsdecode(folder_path)
-os.chdir(f'{decoded_folder_path}')
-os.makedirs('Organizer-Folder/SubFolder-1')
-
-
-'''
-def create_folders(dir_path, FILE_CATEGORIES):
+def create_folders(FILE_CATEGORIES, filepath):
     for key in FILE_CATEGORIES:
-        if key not in 
-    pass
+        if key not in os.listdir(filepath):
+            os.mkdir(os.path.join(filepath, key))
 
 
-def get_files(filepath):
-    for file in os.listdir(filepath):
-        filename = os.fsdecode(file)
-        print(filename)
+
 
 
 if __name__ == "__main__":
-    filepath = sys.argv[0]
-    get_files(filepath)
-
-'''
+    filepath = "C:/Users/oyemi/Downloads"
+    FILE_CATEGORIES = {
+        "PDFs": [".pdf"],
+        "Images": [".jpg", ".jpeg", ".png", ".gif"],
+        "Archives": [".zip", ".7z", ".rar", ".tar", ".gz"],
+        "Code": [".py", ".java", ".cpp", ".c", ".js", ".ts", ".html", ".css"],
+    }
